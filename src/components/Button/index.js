97d656e994db237a20edbfc1e7b556262eeb1c87
Raphael Portela <rafaeljansenaraujo@gmail.com>
2020-03-29 18:58:47 -0300

    button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,29 +6,7 @@ export default function Button({...props}) {
 
 
 
-  const [didClickButton, setDidClickButton] = useState(false)
 
-  useEffect(() => {
-    // if (didClickButton) {
-      document.addEventListener("click", mouseClickHandler)
-    //   return () => {
-    //     document.removeEventListener("click", mouseClickHandler)
-    //   }
-    // } else {
-    //   document.removeEventListener("click", mouseClickHandler)
-    // }
-  }, [didClickButton])
 
-  const buttonClickHandler = () => {
-    setDidClickButton(true)
-    console.log('asd');
-  }
-
-  const mouseClickHandler = (event) => {
-    console.log(event.target)
-    setDidClickButton(false) // If you want to reset the behavior again
-    console.log('asd');
-  }
-
-  return <MyButton {...props}type="button" onClick={buttonClickHandler} ></MyButton>;
+  return <MyButton {...props}type="button" ></MyButton>;
 }
